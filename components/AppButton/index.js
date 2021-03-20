351ba3206091a20947938bcf5f91d2445243b3ad
Aleksander Jess <jess.aleksander@gmail.com>
2021-03-20 15:25:10 +0100

    added a functionality to change buttons colour

--- components/AppButton/index.js
+++ components/AppButton/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import {Pressable} from "react-native";
 import {Platform} from "react-native-web";
 import PropTypes from 'prop-types';
+import tailwind from "tailwind-rn";
 
 import AppText from "../AppText";
 
+
 export default function AppButton(props) {
     const isAndroid = Platform.OS === "android"
+    // determining the color of the button
+    const bgColorInactive = props.bgColor ? tailwind(`bg-${props.bgColor}-${props.brightness}`) : tailwind('bg-blue-700')
+    const bgColorActive = props.bgColor ? tailwind(`bg-${props.bgColor}-${props.brightness + 100}`) : tailwind ('bg-blue-800')
+
+    const backgroundColorInactive = bgColorInactive.backgroundColor;
+    const backgroundColorActive = bgColorActive.backgroundColor;
+
+
     return (
-        <Pressable onPress={props.onPress} android_ripple={isAndroid}>
+        <Pressable style={({pressed}) => [tailwind('p-3 rounded'), {backgroundColor: pressed ? backgroundColorActive : backgroundColorInactive}]} onPress={props.onPress} android_ripple={isAndroid}>
             <AppText>{props.children}</AppText>
         </Pressable>
     )
@@ -17,4 +27,6 @@ export default function AppButton(props) {
 AppButton.propTypes = {
     children: PropTypes.node.isRequired,
     onPress: PropTypes.func.isRequired,
+    bgColor: PropTypes.string,
+    brightness: PropTypes.oneOf([50, 100, 200, 300, 400, 500, 600, 700, 800, 900]),
 };
